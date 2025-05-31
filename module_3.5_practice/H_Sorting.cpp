bdeccd5ea28431f7778ae7c3831b42fc39fb0ba3
Source-Code-007 <utsho926@gmail.com>
2025-05-31 17:56:48 +0600

    3.5 practice problem completed

--- module_3.5_practice/H_Sorting.cpp
+++ module_3.5_practice/H_Sorting.cpp
@@ -8,26 +8,29 @@ int main(){
         for(int i = 0; i<n; i++){
             cin >> arr[i];
         }
+
+
         
-        int sortArr[n];
-        for(int i = 0; i<n; i++){
-            sortArr[i] = arr[i];
-        }
+        sort(arr, arr+n);
+        // int sortArr[n];
+        // for(int i = 0; i<n; i++){
+        //     sortArr[i] = arr[i];
+        // }
 
-        // swap
+        // // swap
+
+        // for(int i = 0; i<n; i++){
+        //     for(int j = i+1; j<n; j++){
+        //         if(sortArr[j] < sortArr[i]){
+        //             int temp = sortArr[j];
+        //             sortArr[j] = sortArr[i];
+        //             sortArr[i] = temp; 
+        //         }
+        //     }
+        // }
 
         for(int i = 0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                if(sortArr[j] < sortArr[i]){
-                    int temp = sortArr[j];
-                    sortArr[j] = sortArr[i];
-                    sortArr[i] = temp; 
-                }
-            }
-        }
-
-        for(int i = 0; i<n; i++){
-            cout << sortArr[i] << " ";
+            cout << arr[i] << " ";
         }
 
 
